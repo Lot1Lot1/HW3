1b25f35269087b3b261c2d2e541e14034653e03c
Lot1Lot1 <2011pzm@mail.ru>
2024-02-28 02:56:54 +0300

    initial commit

---
+++ products/main/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from main.models import Review, Product
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+
+
+class ProductListSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+
+class ProductDetailsSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ('title', 'price','reviews', 'description')
